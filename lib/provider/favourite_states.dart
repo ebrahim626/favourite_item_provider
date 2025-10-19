7f3d626959ea1b31ed054ab77929982b8556df57
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-19 14:43:31 +0600

    updated

---
+++ lib/provider/favourite_states.dart
@@ -0,0 +1,23 @@
+import 'package:favourite_items_provider/model/item.dart';
+
+class FavouriteStates {
+  final List<Item> allItem;
+  final List<Item> favouriteItem;
+  final String search;
+  FavouriteStates({
+    required this.allItem,
+    required this.favouriteItem,
+    required this.search,
+  });
+  FavouriteStates copyWith(
+    List<Item>? allItem,
+    List<Item>? favouriteItem,
+    String? search,
+  ) {
+    return FavouriteStates(
+      allItem: allItem ?? this.allItem,
+      favouriteItem: favouriteItem ?? this.favouriteItem,
+      search: search ?? this.search,
+    );
+  }
+}
