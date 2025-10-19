e6519d072a0d205ab7017b436df49de4bad27d9b
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-19 14:38:49 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:favourite_items_provider/home_screen.dart';
+import 'package:favourite_items_provider/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/model/item.dart
@@ -0,0 +1,11 @@
+
+class Item {
+  final String name;
+  final String id;
+  final bool favourite;
+  Item({required this.id,required this.name,required this.favourite});
+
+  Item copyWith(String? name,String? id,bool? favourite){
+    return Item(id: id ?? this.id, name: name ??this.name, favourite: favourite ?? this.favourite);
+  }
+}
